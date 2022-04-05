29c60f4437f655bf7d5b59e8026950df247acb07
Felistra <felistra102@student.kau.se>
2022-04-05 17:02:06 +0200

    First commit

---
+++ src/Controller.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+import java.awt.event.*;
+
+public class Controller implements ActionListener {
+	private Vy v;
+	
+	public Controller(Vy vin) {
+		v=vin;
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		
+		
+	}
+	
+	
+}
---
+++ src/Program.java
@@ -0,0 +1,9 @@
+
+public class Program {
+
+	public static void main(String[] args) {
+		Vy vy = new Vy(); 
+
+	}
+
+}
